dce70fd19f427597fa1e4df2e20c4e76e693545b
Anthony <anthonyd-m@live.com>
2019-05-07 16:08:00 +0200

    detail

--- front/src/App.js
+++ front/src/App.js
@@ -5,9 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-        </Router>
+      <div>
+        <Router/>
       </div>
     );
   }
