9ca17b017257e20081243331565180e5d2b02f25
Anthony <anthonyd-m@live.com>
2019-05-26 13:48:10 +0200

    filters bdd

--- bdd.sql
+++ bdd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 24, 2019 at 01:32 PM
+-- Generation Time: May 26, 2019 at 11:47 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
@@ -26,16 +26,20 @@ CREATE TABLE `boxes` (
   `price` float NOT NULL,
   `description` text NOT NULL,
   `animal` varchar(25) DEFAULT NULL,
-  `img_name` varchar(50) NOT NULL
+  `img_name` varchar(50) NOT NULL,
+  `age` varchar(50) NOT NULL,
+  `weight` varchar(50) NOT NULL,
+  `fur` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `boxes`
 --
 
-INSERT INTO `boxes` (`id_box`, `name`, `price`, `description`, `animal`, `img_name`) VALUES
-(1, 'test', 20, 'Box de test.', 'chien', 'test1.jpg'),
-(2, 'produit de test', 50, 'un produit pour tester', 'chat', 'test2.jpg');
+INSERT INTO `boxes` (`id_box`, `name`, `price`, `description`, `animal`, `img_name`, `age`, `weight`, `fur`) VALUES
+(1, 'test', 20, 'Box de test.', 'chien', 'test1.jpg', 'adulte', 'souspoids', 'long'),
+(2, 'produit de test', 50, 'un produit pour tester', 'chat', 'test2.jpg', 'bebe', 'normal', 'court'),
+(3, 'Lasagnes', 30, 'Un délicieux plat de lasagnes italiennes pour votre chat', 'chat', 'test2.jpg', 'senior', 'surpoids', 'long');
 
 -- --------------------------------------------------------
 
@@ -115,7 +119,8 @@ INSERT INTO `orders` (`id_order`, `status`, `user_id`, `box_id`, `adress`, `date
 (13, 'ordered', 4, 1, '24 rue Henri Martin', '2019-05-24', NULL, 1),
 (14, 'ordered', 4, 1, 'adresse', '2019-05-24', NULL, 1),
 (15, 'ordered', 4, 1, '', '2019-05-24', NULL, 1),
-(16, 'ordered', 4, 1, 'adresse', '2019-05-24', NULL, 1);
+(16, 'ordered', 4, 1, 'adresse', '2019-05-24', NULL, 1),
+(17, 'ordered', 4, 1, 'adresse', '2019-05-26', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -129,15 +134,17 @@ CREATE TABLE `recipes` (
   `steps` longtext NOT NULL,
   `preparation_time` varchar(10) NOT NULL,
   `cook_time` varchar(10) NOT NULL,
-  `difficulty` int(11) NOT NULL
+  `difficulty` int(11) NOT NULL,
+  `id_box` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `recipes`
 --
 
-INSERT INTO `recipes` (`id_recipe`, `name`, `steps`, `preparation_time`, `cook_time`, `difficulty`) VALUES
-(1, 'test', '1. premiere etape\r\n2. deuxieme etape\r\n3. troisieme etape', '10h', '2h', 0);
+INSERT INTO `recipes` (`id_recipe`, `name`, `steps`, `preparation_time`, `cook_time`, `difficulty`, `id_box`) VALUES
+(1, 'test', '1. premiere etape\r\n2. deuxieme etape\r\n3. troisieme etape', '10h', '2h', 0, 1),
+(2, 'test', '1. premiere etape\r\n2. deuxieme etape\r\n3. troisieme etape', '10h', '2h', 0, 2);
 
 -- --------------------------------------------------------
 
@@ -174,7 +181,8 @@ CREATE TABLE `recipe_has_tools` (
 --
 
 INSERT INTO `recipe_has_tools` (`tool_id`, `recipe_id`) VALUES
-(1, 1);
+(1, 1),
+(2, 1);
 
 -- --------------------------------------------------------
 
@@ -211,7 +219,8 @@ CREATE TABLE `tools` (
 --
 
 INSERT INTO `tools` (`id_tool`, `tool`) VALUES
-(1, 'fouet');
+(1, 'fouet'),
+(2, 'bol');
 
 -- --------------------------------------------------------
 
@@ -303,7 +312,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `boxes`
 --
 ALTER TABLE `boxes`
-  MODIFY `id_box` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_box` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `feedbacks`
@@ -321,13 +330,13 @@ ALTER TABLE `ingredients`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_order` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `recipes`
 --
 ALTER TABLE `recipes`
-  MODIFY `id_recipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_recipe` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tokens`
@@ -339,7 +348,7 @@ ALTER TABLE `tokens`
 -- AUTO_INCREMENT for table `tools`
 --
 ALTER TABLE `tools`
-  MODIFY `id_tool` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_tool` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
